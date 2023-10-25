7d5344a371da76924156c9c7249aa6aa34bfdc4b
Rafael de Sá <rafaeldesa170622@gmail.com>
2023-10-25 08:02:58 -0300

    update - Produtos

--- src/Classes/Produto.java
+++ src/Classes/Produto.java
@@ -4,10 +4,77 @@
  */
 package Classes;
 
+import Interfaces.*;
+
 /**
  *
  * @author rafae
  */
-public class Produto {
+public class Produto implements IInterface {
 
+    private String nome;
+    private double valorCusto;
+    private Fornecedor fornecedor;
+    private double valorVenda;
+    private boolean status;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValorCusto() {
+        return valorCusto;
+    }
+
+    public void setValorCusto(double valorCusto) {
+        this.valorCusto = valorCusto;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public double getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(double valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public void cadastrar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void alterar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void excluir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void cancelar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
